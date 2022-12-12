ae3315c1fb73219e4431d88a56b78dd0a9eb6776
Dowan <shukang91@gmail.com>
2022-12-12 10:33:02 +0900

    12/12 f test

---
+++ Source/YGL_MultiProject/YGL_MultiProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "YGL_MultiProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YGL_MultiProject, "YGL_MultiProject" );
